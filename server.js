267e6d00c88edd95966d6e2ab86496f9dd3fc789
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-08-01 17:31:47 +0100

    product api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const Product = require("./modules/productModule.js")
 
-// Node Product API 
+// Product API 
 
 const app = express()
 app.use(express.json())
